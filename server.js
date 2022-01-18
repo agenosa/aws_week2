d5e272e3cebf5a5bace8d5ab769dc1c8b2b6c1bf
agenosa <agenosa@my.bcit.ca>
2022-01-18 15:11:47 -0800

    added frontend code

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const multer = require('multer')
 const fs = require('fs')
+const path = require('path')
+require('dotenv').config()
+
+const database = require('./database')
 
 const app = express()
 
 const upload = multer({ dest: 'images/'})
+
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get("/", (req, res) => {
     res.send("my app!")
@@ -15,20 +21,22 @@ app.get("/", (req, res) => {
 
 app.get('/images/:imageName', (req, res) => {
     const imageName = req.params.imageName
-  
     const readStream = fs.createReadStream(`images/${imageName}`)
     readStream.pipe(res)
 })
 
+app.get('/api/images', async (req, res) => {
+    const images = await database.getImages()
+    res.send({images})
+} )
 
 
-app.post('/api/images', upload.single('image'), (req, res) => {
+app.post('/api/images', upload.single('image'), async (req, res) => {
     const imagePath = req.file.path
     const description = req.body.description
-    console.log(description, imagePath, req.file);
-    res.send({description, imagePath})
+    const image = await database.addImage(imagePath, description)
+    res.send({image})
 })
-
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`listening on port ${port}`))
